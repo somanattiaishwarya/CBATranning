38ca21c282ca70c3ea9f25e0f0fd7e4770875915
AbhijitDynamo <42991032+AbhijitDynamo@users.noreply.github.com>
2019-05-08 13:52:08 +0530

    Update abhijit.c

--- abhijit.c
+++ abhijit.c
@@ -2,10 +2,10 @@
 main()
 {
 int number,i;
-printf("Enter the number: \n");
-scanf(" %d",&number)
+printf("Enter the number: ");
+scanf(" %d",&number);
 for(i=1;i<=10;i++)
   {
-    printf("%2d X %2d = %2d\n",number,i,(number*i))
+    printf("%2d X %2d = %2d\n",number,i,(number*i));
   }
 }
