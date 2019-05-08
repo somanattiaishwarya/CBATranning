bfbe35dde14b29cc9e41de2abf226eb84e521202
Sheregarpooja <43609819+Sheregarpooja@users.noreply.github.com>
2019-05-08 15:42:34 +0530

    Update pooja.c

--- pooja.c
+++ pooja.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
-#include <math.h>
-
+#include<stdio.h>
 int main()
-{    
-    float num=6 , cubeRoot;
-    //printf("enter a number");
-    //scanf("%f",&num);
-    
-
-    cubeRoot =  cbrt(num);
-    printf("Cube root of %f =  %f", num, cubeRoot);
-
-    return 0; 
-} 
+{
+  int c, n=9, fact = 1;
+ 
+  //printf("Enter a number to calculate its factorial\n");
+  //scanf("%d", &n);
+ 
+  for (c = 1; c <= n; c++)
+    fact = fact * c;
+ 
+  printf("Factorial of %d = %d\n", n, fact);
+ 
+  return 0;
+}
