8797a1f4c001a212cd46c0d427db5675dde0ed45
somanattiaishwarya <50397144+somanattiaishwarya@users.noreply.github.com>
2019-05-08 15:24:05 +0530

    Update aish.c

--- aish.c
+++ aish.c
@@ -7,17 +7,17 @@
 
 int main()
 {
-    float num, root;
+    float num=144, root;
 
     /* Input a number from user */
-    printf("Enter any number to find square root: ");
-    scanf("%f", &num);
+   // ("Enter any number to find square root: ");
+//("%f", &num);
 
     /* Calculate square root of num */
     root = sqrt(num);
 
     /* Print the resultant value */
-    printf("Square root of %.2f = %.2f", num, root);
+    ("Square root of %.2f = %.2f", num, root);
 
     return 0;
 }
